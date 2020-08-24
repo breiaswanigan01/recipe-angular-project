977109eebb72aa9b6b3c2bbad31c082d5adb35b0
Kanchan <kanchankandage@gmail.com>
2020-08-24 19:43:00 -0400

    removed checkboxes and added radio functions

--- src/app/recipes-home/recipes-home.component.ts
+++ src/app/recipes-home/recipes-home.component.ts
@@ -1,51 +1,56 @@
-import { Component, OnInit } from '@angular/core';
-import { Hit } from '../interfaces/recipe';
-import { ActivatedRoute } from '@angular/router';
-import { RecipesService } from '../recipes.service';
-import { range } from 'rxjs';
+import { Component, OnInit } from "@angular/core";
+import { Hit } from "../interfaces/recipe";
+import { ActivatedRoute } from "@angular/router";
+import { RecipesService } from "../recipes.service";
+import { range } from "rxjs";
 
 @Component({
-  selector: 'app-recipes-home',
-  templateUrl: './recipes-home.component.html',
-  styleUrls: ['./recipes-home.component.css'],
+	selector: "app-recipes-home",
+	templateUrl: "./recipes-home.component.html",
+	styleUrls: ["./recipes-home.component.css"],
 })
 export class RecipesHomeComponent implements OnInit {
-  recipeArray: Hit[];
-  constructor(private route: ActivatedRoute, private edamam: RecipesService) {}
+	recipeArray: Hit[];
+	constructor(private route: ActivatedRoute, private edamam: RecipesService) {}
 
-  ngOnInit(): void {
-    this.getRecipes();
-  }
+	ngOnInit(): void {
+		this.getRecipes();
+	}
 
-  getRecipes = () => {
-    // console.log('getRecipes');
-    // console.log(recipes);
+	getRecipes = () => {
+		// console.log('getRecipes');
+		// console.log(recipes);
 
-    // this.recipeArray = recipes;
-    this.route.queryParamMap.subscribe((params) => {
-      let term = params.get('term');
-      let dietOptions = params.getAll('dietLabels');
-      let minCal = params.get('minCal');
-      let maxCal = params.get('maxCal');
-    //   console.log(params.getAll("dietOptions"));
-      console.log(
-        `term : ${term} dietOptions : ${dietOptions}, minCal: ${minCal}, maxCal: ${maxCal}`
-      );
-      if (minCal !== '' && maxCal !== '') {
-        let calorieRange = `${minCal}-${maxCal}`;
-        console.log('print calorie range', calorieRange);
-        this.edamam
-          .getRecipesWithCalories(term, calorieRange)
-          .subscribe((response) => {
-            console.log('subscribe calories', response);
-            this.recipeArray = response.hits;
-          });
-      } else {
-        this.edamam.getRecipes(term).subscribe((response) => {
-          console.log('*********** ', response);
-          this.recipeArray = response.hits;
-        });
-      }
-    });
-  };
+		// this.recipeArray = recipes;
+		this.route.queryParamMap.subscribe((params) => {
+			let term = params.get("term");
+			let diet = params.get("diet");
+			let minCal = params.get("minCal") === "" ? "0" : params.get("minCal");
+			let maxCal = params.get("maxCal");
+			//   console.log(params.getAll("dietOptions"));
+			console.log(
+				`term : ${term} dietOptions : ${diet}, minCal: ${minCal}, maxCal: ${maxCal}`
+			);
+			if (minCal !== "" && maxCal !== "" && diet === "None") {
+				let calorieRange = `${minCal}-${maxCal}`;
+				console.log("print calorie range", calorieRange);
+				this.edamam
+					.getRecipesWithCalories(term, calorieRange)
+					.subscribe((response) => {
+						console.log("subscribe calories", response);
+						this.recipeArray = response.hits;
+					});
+			} else if (maxCal === "" && diet !== "None") {
+				this.edamam.getRecipesWithDiet(term, diet).subscribe((response) => {
+					console.log("subscribe diet", response);
+					this.recipeArray = response.hits;
+				});
+			} else {
+				this.edamam.getRecipes(term).subscribe((response) => {
+					console.log("*********** ", response);
+					this.recipeArray = response.hits;
+				});
+			}
+		});
+	};
 }
