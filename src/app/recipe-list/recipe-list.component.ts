7aa09fb71eb4db154b169ef1c7a556d5cc646511
breen1em <erin.breen@icloud.com>
2020-08-25 16:38:05 -0400

    pop-up

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,22 +1,26 @@
-import { Component, OnInit, Input } from "@angular/core";
-import { Recipe } from "../interfaces/recipe";
-import { RecipesService } from "../recipes.service";
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { Recipe } from '../interfaces/recipe';
+import { RecipesService } from '../recipes.service';
 
 @Component({
-	selector: "app-recipe-list",
-	templateUrl: "./recipe-list.component.html",
-	styleUrls: ["./recipe-list.component.css"],
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-	@Input() recipesRef: Recipe[];
+  @Input() recipesRef: Recipe[];
+  @Output() quickView = new EventEmitter<number>();
 
-	favoriteArray: Recipe[] = [];
-	constructor(private service: RecipesService) {}
+  favoriteArray: Recipe[] = [];
+  constructor(private service: RecipesService) {}
 
-	ngOnInit(): void {}
+  ngOnInit(): void {}
 
-	addToFavorites = (recipe: Recipe) => {
-		console.log("addToFav");
-		this.service.addToFavorites(recipe);
-	};
+  addToFavorites = (recipe: Recipe) => {
+    console.log('addToFav');
+    this.service.addToFavorites(recipe);
+  };
+  showDetails = (index: number) => {
+    this.quickView.emit(index);
+  };
 }
